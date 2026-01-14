3e10a74be6432f3334c39e8fc6f8ad72465221e8
Shravani Bele <shravanibele1008@gmail.com>
2026-01-14 17:15:36 +0530

    added conversion file

---
+++ ConversionObject.js
@@ -0,0 +1,43 @@
+// let num = 33;
+// let num_string = String(num);
+// console.log(num);
+// console.log(typeof(num_string));
+
+// let numString= "33";
+// let num1= Number(numString);
+// console.log(num1);
+// console.log(typeof(num1));
+
+// let jj="33abc";
+// let num_string = Number(jj);
+// console.log(jj);
+// console.log(num_string);
+
+// //operations
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2/2);
+//  console.log(2%2);
+
+// let str1 ="Hello ";
+// let str2= "Shravani";
+// console.log(str1 + str2);
+
+// console.log("1"+2);
+// console.log("1"+ 2+2);
+// console.log(1 +"2");
+// console.log(1+2+ "2");
+
+// console.log(true);        // Activity 2
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+let score = 200;
+++score;
+console.log(score);
+
